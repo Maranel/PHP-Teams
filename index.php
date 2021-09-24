0990d4ef855eaede86925881abc10e4d9ec9cdd2
MSOK <mario.sokolovsky@gmail.com>
2021-09-24 20:45:35 +0200

    Added styles

--- index.php
+++ index.php
@@ -22,6 +22,7 @@
                 <form action="form.php" method="post" class="card">
                     <div class="card-body">
                         <h4>Welcome to the system that will change your LIFE!</h4>
+                        <p><small>Please register!</small></p>
                         <div class="mb-3">
                             <label for="username" class="form-label">Meno:</label>
                             <input type="text" class="form-control" id="username" name="username" placeholder="Meno">
